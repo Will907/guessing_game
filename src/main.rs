0655141e7cf048f1fe85bcbc1bd8a5a4d5e38b49
will <will.pinder.2006@gmail.com>
2024-09-14 17:15:42 +0100

    Finished the comparison and fixed error handling

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,28 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("Number guessing game");
-    println!("Guess a number from 1-100: ");
-    let mut guess = String::new();
     let secret = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is {secret}");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess: u32 = guess.trim().parse().expect("Not a number, try again.");
-    println!("You guessed: {guess}");
+    loop {
+        println!("Guess a number from 1-100: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Equal => println!("You guessed it!"),
-        Ordering::Greater => println!("Too big"),
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Equal => {
+                println!("You guessed it!");
+                break;
+            },
+            Ordering::Greater => println!("Too big"),
+        }
     }
 }
