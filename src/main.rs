1ef1401ae9c2cc4e83fea18f7277a2a8d3a925b6
will <will.pinder.2006@gmail.com>
2024-09-14 17:02:40 +0100

    Added rng functionality

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Number guessing game");
@@ -7,9 +8,16 @@ fn main() {
     let mut guess = String::new();
     let secret = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is {secret}");
+
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    let guess: u32 = guess.trim().parse().expect("Not a number, try again.");
     println!("You guessed: {guess}");
 
+    match guess.cmp(&secret) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Equal => println!("You guessed it!"),
+        Ordering::Greater => println!("Too big"),
+    }
 }
